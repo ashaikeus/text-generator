0fca906756817bc44654e23135ef5a967fd36a43
shi-anli <suloversanja@gmail.com>
2021-09-12 03:07:15 +0300

    Update text_generator.py

--- generators/text_generator.py
+++ generators/text_generator.py
@@ -4,12 +4,12 @@ from nltk.util import bigrams
 
 
 def initialize(filename):
-    global tokens, trigram_list, SENTENCE_ENDERS, word
+    global tokens, bigram_list, SENTENCE_ENDERS, word
     file = open(filename, "r", encoding="utf-8")
     tokens = []
     for line in file:
         tokens += regexp_tokenize(line, r"\S+")
-    trigram_list = list(bigrams(tokens))
+    bigram_list = list(bigrams(tokens))
     SENTENCE_ENDERS = ".?!"
     word = "."
 
@@ -18,7 +18,7 @@ def start_a_sentence():
     global current_sent_length, word, sentence
     current_sent_length = 0
     while word[0][0] != word[0][0].upper() or word.split()[0][-1] in SENTENCE_ENDERS or word[0][0] == "-":
-        word = random.choice(trigram_list)[0]
+        word = random.choice(bigram_list)[0]
     sentence = word.split()
 
 
@@ -27,7 +27,7 @@ def generate():
     start_a_sentence()
     while True:
         current_sent_length += 1
-        word_dict = [t[1] for t in trigram_list if t[0] == " ".join(sentence[current_sent_length - 1:current_sent_length + 1])]
+        word_dict = [t[1] for t in bigram_list if t[0] == " ".join(sentence[current_sent_length - 1:current_sent_length + 1])]
         word = "".join(random.choices(word_dict))
         if word[-1] in SENTENCE_ENDERS:
             iters = 0
